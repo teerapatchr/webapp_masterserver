121c2b8326e649e593017b30f3e422732f07c96e
teerapatchr <teerapatchr@gmail.com>
2026-03-19 14:36:06 +0700

    Test cors

--- Backend/index.js
+++ Backend/index.js
@@ -7,6 +7,16 @@ dotenv.config();
 
 const app = express();
 app.use(cors());
+app.use(
+  cors({
+    origin: [
+      "http://localhost:3000",
+      "https://your-frontend-domain.vercel.app",
+    ],
+    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+  })
+);
+
 app.use(express.json());
 
 const { Pool } = pg;
