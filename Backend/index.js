4fdbd576f0b22533c57190e8650fc640816f422b
teerapatchr <teerapatchr@gmail.com>
2026-03-19 14:32:38 +0700

    Test fix bug Backend

--- Backend/index.js
+++ Backend/index.js
@@ -6,7 +6,16 @@ import pg from "pg";
 dotenv.config();
 
 const app = express();
-app.use(cors());
+
+app.use(
+  cors({
+    origin: [
+      "http://localhost:3000",
+      "https://your-frontend-domain.vercel.app",
+    ],
+    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+  })
+);
 app.use(express.json());
 
 const { Pool } = pg;
